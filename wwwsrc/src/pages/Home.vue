886f5a202c67f45a9f07b8820963401016461593
JustinBarnard1 <jbarnard0031@gmail.com>
2020-11-16 13:53:24 -0700

    Home started

--- wwwsrc/src/pages/Home.vue
+++ wwwsrc/src/pages/Home.vue
@@ -1,11 +1,53 @@
 <template>
-  <div class="home">
-    <h1>Welcome</h1>
+  <div class="home m-4">
+    <div class="row">
+      <div class="col-s-12">
+        <h3>
+          Welcome To WC Where You Can Create Your Own Wellness Challenges And
+          Share Them With Your Friends!
+          <button type="button" class="btn btn-primary" @click="login">
+            Get Started
+          </button>
+        </h3>
+      </div>
+    </div>
+    <img class="main-img mt-3" src="../assets/wellness.png" alt="" />
   </div>
 </template>
 
 <script>
+import { getUserData } from "@bcwdev/auth0-vue";
+import { setBearer, resetBearer } from "../services/AxiosService";
 export default {
   name: "home",
+  computed: {
+    profile() {
+      return this.$store.state.profile;
+    },
+  },
+  methods: {
+    async login() {
+      await this.$auth.loginWithPopup();
+      if (this.$auth.isAuthenticated) {
+        setBearer(this.$auth.bearer);
+        this.$store.dispatch("getProfile");
+      }
+    },
+  },
+  watch: {
+    profile: function (userProfile) {
+      if (userProfile) {
+        router.push({ name: "Dashboard" });
+      } else {
+        return;
+      }
+    },
+  },
 };
 </script>
+<style scoped>
+.main-img {
+  max-width: 90vw;
+  max-height: 50vh;
+}
+</style>
