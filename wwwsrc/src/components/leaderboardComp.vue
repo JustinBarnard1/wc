f659e0d3fabb3fe1f92f2d307d5fd55719479371
JustinBarnard1 <jbarnard0031@gmail.com>
2020-11-17 12:23:26 -0700

    leaderboardComp

---
+++ wwwsrc/src/components/leaderboardComp.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="leaderboardComp"></div>
+</template>
+
+
+<script>
+export default {
+  name: "leaderboardComp",
+  data() {
+    return {};
+  },
+  computed: {},
+  methods: {},
+  components: {},
+};
+</script>
+
+
+<style scoped>
+</style>
