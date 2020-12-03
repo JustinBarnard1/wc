4f7f2680b53fd15d15c46d5d19a2f3701d4890f0
JustinBarnard1 <jbarnard0031@gmail.com>
2020-12-03 14:46:43 -0700

    challengesController create started

--- Controllers/ChallengesController.cs
+++ Controllers/ChallengesController.cs
@@ -1,4 +1,8 @@
+using System.Threading.Tasks;
+using CodeWorks.Auth0Provider;
+using Keepr.Models;
 using Keepr.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Keepr.Controllers
@@ -11,6 +15,22 @@ namespace Keepr.Controllers
         public ChallengesController(ChallengesService cs)
         {
             _cs = cs;
+        }
+    }
+
+    [HttpPost]
+    [Authorize]
+    public async Task<ActionResult<Challenge>> Create([FromBody] Challenge newChallenge)
+    {
+        try
+        {
+            Profile userInfo = await HttpContext.GetUserInfoAsync<Profile>();
+            newChallenge.CreatorId = userInfo.Id;
+            Challenge created = _cs.Create(userInfo.Id, newChallenge);
+        }
+        catch (System.Exception e)
+        {
+            return BadRequest(e.Message);
         }
     }
 }
