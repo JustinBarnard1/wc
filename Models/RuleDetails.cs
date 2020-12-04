9e802a7fd6744a65e0524f50616872fba30f8714
JustinBarnard1 <jbarnard0031@gmail.com>
2020-12-04 11:04:15 -0700

    changed RuleDetails model for no more Rule model

--- Models/RuleDetails.cs
+++ Models/RuleDetails.cs
@@ -3,9 +3,9 @@ namespace Keepr.Models
     public class RuleDetails
     {
         public int Id { get; set; }
-        public string RuleId { get; set; }
         public string ChallengeId { get; set; }
         public string CreatorId { get; set; }
+        public string Title { get; set; }
         public string Description { get; set; }
         public int MinPoint { get; set; }
         public int MaxPoint { get; set; }
