6b32b0bf12971d9e04ffbe89e2904903d823d9db
JustinBarnard1 <jbarnard0031@gmail.com>
2020-12-08 13:25:50 -0700

    removed totaldailypoints

--- Models/DailyPoints.cs
+++ Models/DailyPoints.cs
@@ -5,7 +5,7 @@ namespace Keepr.Models
         public int Id { get; set; }
         public string RulesId { get; set; }
         public string ChallengeId { get; set; }
-        public string TotalDailyPointsId { get; set; }
+        public string ParticipantId { get; set; }
         public int Day { get; set; }
         public int Points { get; set; }
     }
