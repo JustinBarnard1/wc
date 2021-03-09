4d35b8eb0bdcb87a66e5754b09baa2a0d12ad440
justinbarnard <jbarnard0031@gmail.com>
2021-03-09 10:28:12 -0700

    Started UpdatePoints in DPController

--- Controllers/DailyPointsController.cs
+++ Controllers/DailyPointsController.cs
@@ -1,3 +1,7 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using CodeWorks.Auth0Provider;
+using Keepr.Models;
 using Keepr.Services;
 using Microsoft.AspNetCore.Mvc;
 
@@ -15,5 +19,21 @@ namespace Keepr.Controllers
 
         //ANCHOR Create Daily Point Sheets for each day of the Challenge
         //ANCHOR after a Participant has been accepted into the Challenge
+        //ANCHOR this part is in DPSServices
+
+        //ANCHOR Changes point totals for one or many days at a time
+        public async Task<ActionResult<IEnumerable<DailyPoints>>> UpdatePoints(int id, [FromBody] DailyPoints pointsUpdate)
+        {
+            try
+            {
+                Profile userInfo = await HttpContext.GetUserInfoAsync<Profile>();
+                pointsUpdate.Id = id;
+                return Ok();
+            }
+            catch (System.Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
     }
 }
