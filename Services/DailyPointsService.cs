c5464565d056e2d1c41a6a6b38b9ef0b3481aaba
justinbarnard <jbarnard0031@gmail.com>
2021-09-14 10:32:00 -0600

    DailyPointsService

--- Services/DailyPointsService.cs
+++ Services/DailyPointsService.cs
@@ -1,4 +1,5 @@
 using Keepr.Repositories;
+using Keepr.Models;
 
 namespace Keepr.Services
 {
@@ -8,6 +9,15 @@ namespace Keepr.Services
         public DailyPointsService(DailyPointsRepository repo)
         {
             _repo = repo;
+        }
+
+        //ANCHOR Creates Daily Points Sheets Or DPS
+        //ANCHOR Needs to create all Daily Point Sheets for
+        //ANCHOR all the accepted participants
+        internal DailyPoints Create(DailyPoints newDPS)
+        {
+            newDPS.Id = _repo.Create(newDPS);
+            return newDPS;
         }
     }
 }
